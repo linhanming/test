4a725bd58be9af567dcb60a82f691bf9518f71b1
linhanming <1094366169@qq.com>
2020-05-14 23:31:30 +0800

    master 提交

--- aa.php
+++ aa.php
@@ -2,14 +2,3 @@
 666666666666
 Creating a new branch is quick.
 Creating a new branch is quick.
-Creating a new branch is quick.
-<<<<<<< HEAD
-Creating a new branch is quick.
-=======
-Creating a new branch is quick.
-Creating a new branch is quick.
-Creating a new branch is quick.
-Creating a new branch is quick.
-Creating a new branch is quick.
-Creating a new branch is quick.
->>>>>>> feature1
