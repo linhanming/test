bde30d104200c723d913e01ea4d5fbcc44902d13
linhanming <1094366169@qq.com>
2020-05-17 01:12:00 +0800

    新增文件66666666

--- b/b.php
+++ b/b.php
@@ -2,6 +2,12 @@
 print_r(121231321);
 print_r('是b.php文件');
 // phpinfo();
+function htl() {
+    print_r(rand(1, 10000));
+    $aa = '';
+    $aa = '';
+    $aa = '';
+}
 
-print_r(rand(1, 10000));
+
 ?>
